04792b57c0d6b65156007fa2753e0b7d857ed8ba
alexandre janniaux <alexandre.janniaux@telecom-paristech.org>
2016-12-27 00:34:10 +0100

    Add generic views for article.

--- article/views.py
+++ article/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from .models import Article
+
+class ArticleDetail(DetailView):
+    model = Article
+
+class ArticleList(ListView):
+    model = Article
+
+    
