03f98c9e421e052b3f3226872e3be7f8e8c58ff0
Marcenat <adrien.marcenat@telecom-paristech.fr>
2016-12-30 12:17:24 +0100

    add pseudo views 'detail' and 'list'

--- article/views.py
+++ article/views.py
@@ -11,4 +11,8 @@ class ArticleDetail(DetailView):
 class ArticleList(ListView):
     model = Article
 
-    
+def detail(request, article_title):
+    return HttpResponse("You're reading article %s." % article_title)
+
+def list(request):
+    return HttpResponse("You're looking at the article list")
