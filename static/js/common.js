15e882a98a0b641081fb33d0cd15827ee09cfe92
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-01-31 19:08:06 +0100

    add static/sj/common.js

---
+++ static/js/common.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
+
+});
+
+(function ($) {
+    $(document).ready(function() {
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+
+                // set distance user needs to scroll before we start fadeOut
+                if ($(this).scrollTop() > 300) {
+                    $('.navbar').fadeOut();
+                } else {
+                    $('.navbar').fadeIn();
+                }
+            });
+        });
+    });
+}(jQuery));
+
+$(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+});
