c3a57c934ea146c0ec1e640b64d756c1076ed24e
Marcenat <adrien.marcenat@telecom-paristech.fr>
2017-02-05 23:48:14 +0100

    implement search request WIP

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search/$', views.search, name='search'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from article.models import Article
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 def index(request):
     article_list = Article.objects.all().order_by('-pub_date')
@@ -9,3 +10,14 @@ def index(request):
         'article_list': article_list
     }
     return render(request, 'pages/index.html', context)
+
+
+def search(request):
+    if request.method == 'POST':
+        vector = SearchVector('title')
+        query = SearchQuery(request.POST.get('search'))
+        article_list = Article.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+        context = {
+                'article_list': article_list
+        }
+        return render(request, 'pages/index.html', context)
