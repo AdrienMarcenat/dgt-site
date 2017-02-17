4333aaec6fc1a9648ca42a5d089560fd3bdf25ac
alexandre janniaux <alexandre.janniaux@gmail.com>
2017-02-17 14:33:10 +0100

    squashed migrations

--- article/models.py
+++ article/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 
+from django.contrib.auth.models import User
 
 class Article(models.Model):
 
--- authorProfile/models.py
+++ authorProfile/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 
-class AuthorProfile(models.Model):
 
-    user = models.ForeignKey(User, verbose_name=_('author'))
+class AuthorProfile(models.Model):
+    
+    user = models.ForeignKey(User, default="", on_delete=models.CASCADE)
     description = models.TextField(verbose_name=_('description'))
 
     def __str__(self):
